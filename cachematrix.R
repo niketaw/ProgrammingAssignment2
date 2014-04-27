ff7ee1b28dccc6f7b459a54eb33a6653338a778a
niketaw <dr.niketa143@gmail.com>
2014-04-27 18:32:30 -0400

    Completed Peer Assessment

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,19 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  inv_mat<-NULL
+  set <- function(y) {
+    mat <<- y
+    inv_mat <<- NULL
+  }
+  get <- function() mat
+  setinv <- function(inv_mat_new) inv_mat <<- inv_mat_new
+  getinv <- function() inv_mat
+  list(set = set, get = get,
+       setinv  = setinv ,
+       getinv = getinv)
+  
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_mat <- x$getinv()
+  if(!is.null(inv_mat)) {
+    message("getting cached data")
+    return(inv_mat)
+  }
+  data <- x$get()
+  inv_mat<- solve(data, ...)
+  x$setinv(inv_mat)
+  inv_mat
 }
