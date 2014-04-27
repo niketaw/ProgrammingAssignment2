eb4d5288413ed2078aab042f5e1aa2b520e7a47f
niketaw <dr.niketa143@gmail.com>
2014-04-27 18:51:11 -0400

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(mat = matrix()) {
-  inv_mat<-NULL
-  set <- function(y) {
+##Take input a matrix and generate a list of four functions
+makeCacheMatrix <- function(mat = matrix()) {     #creates a matrix object
+  inv_mat<-NULL                     #Sets the initial value to NULL for inverse matrix
+  set <- function(y) {              #Add new matrix to makeCacheMatrix list
     mat <<- y
     inv_mat <<- NULL
   }
-  get <- function() mat
-  setinv <- function(inv_mat_new) inv_mat <<- inv_mat_new
-  getinv <- function() inv_mat
-  list(set = set, get = get,
+  get <- function() mat             #Outputs the existing matrix
+  setinv <- function(inv_mat_new) inv_mat <<- inv_mat_new #adds the inverse matrix to the list
+  getinv <- function() inv_mat      #Outputs the inverse matrix of the input matrix
+  list(set = set, get = get,        #Generates the list of four functions
        setinv  = setinv ,
        getinv = getinv)
   
 }
 
 
-## Write a short comment describing this function
+##Get the inverse of the input matrix if in cache else solve the matrix, output the inverse and save it in cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {    # Return a matrix that is the inverse of 'x'
   inv_mat <- x$getinv()
   if(!is.null(inv_mat)) {
     message("getting cached data")
